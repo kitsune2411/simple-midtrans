58b7f1adec5353d4f232142cb695232bc0cb00f1
AgungAwan <agungkristiawan24@gmail.com>
2023-06-24 17:19:29 +0800

    feat(createTransaction): handle err

--- src/controllers/createTransaction.js
+++ src/controllers/createTransaction.js
@@ -98,7 +98,9 @@ module.exports = async (req, res) => {
     return res.status(200).send(result);
   } catch (error) {
     console.log(error);
-    return res.status(500).end();
+    const errMessage =
+      error?.ApiResponse?.error_messages ?? "Internal Server Error";
+    return res.status(500).send(errMessage);
   }
 };
 
